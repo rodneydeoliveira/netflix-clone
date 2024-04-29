2236ad2ef49fadeb8528daf345ac2ea44906d500
Rodney Lucas V. Oliveira <rodneylucas.oliveira@gmail.com>
2024-04-28 22:18:22 -0300

    add: prisma schema

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/input";
 import { useCallback, useState } from "react";
+import axios from "axios";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -12,6 +13,18 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const register = useCallback(async () =>{
+    try {
+      await axios.post('/api/register' , {
+        email,
+        name,
+        password
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },[])
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
